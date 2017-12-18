cf616a628a79fa3a9a35f3a31e5766f6a360e9ba
zhenxingliu <5210167@qq.com>
2017-12-18 11:01:51 +0800

    init projects

---
+++ NSFetchedResultsController/NSFetchedResultsController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSFetchedResultsController
+//
+//  Created by 刘振兴 on 2017/12/18.
+//  Copyright © 2017年 zoneland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
